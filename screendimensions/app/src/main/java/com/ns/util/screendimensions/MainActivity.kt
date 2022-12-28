0519bd867916b34522634655b14c73bae67bb6c3
niralhs1 <niralhs1@users.noreply.github.com>
2022-12-27 16:28:02 -0800

    Initial Commit

---
+++ screendimensions/app/src/main/java/com/ns/util/screendimensions/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.ns.util.screendimensions
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var textView : TextView = findViewById(R.id.text_view_id)//in onCreate()
+
+
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        val width = displayMetrics.widthPixels.toString()
+        val height = displayMetrics.heightPixels.toString()
+        textView.setText("Screen Pixel Dimensions:\n $width(width) X $height(height)");
+    }
+}
